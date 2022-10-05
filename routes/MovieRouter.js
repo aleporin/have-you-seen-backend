0da9e26273442716b27bfbc861b8e68e8c113f52
Steve Morello <smorello4@gmail.com>
2022-10-04 20:28:14 -0400

    attempt no user subclass issue in movie. module.exports movie and show routers

--- routes/MovieRouter.js
+++ routes/MovieRouter.js
@@ -4,3 +4,5 @@ const controllers = require('../controllers/MovieController')
 router.post('/', controllers.GetUserMovies)
 router.post('/create/:user_id', controllers.CreateMovie)
 router.get('/delete/:recipeId', controllers.DeleteMovie)
+
+module.exports = router
